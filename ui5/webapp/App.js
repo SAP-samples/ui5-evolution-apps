adfb4318fd7da173f9b36f660671a9a9fc03a295
Johannes Gluch <johannes.gluch@sap.com>
2023-07-04 11:54:32 +0200

    [INTERNAL] Add poc-standard-apps to repo

---
+++ ui5/webapp/App.js
@@ -0,0 +1,10 @@
+//create AppComponent
+sap.ui.require([
+	"sap/ui/core/ComponentContainer"
+], function(ComponentContainer) {
+	new ComponentContainer({
+		id: "compCont",
+		name: "sap.ui.demo.todo",
+		manifest: true
+	}).placeAt("content");
+},function(oError) {console.log(oError);});
