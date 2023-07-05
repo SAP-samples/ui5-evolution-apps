3b24c1743d74cc1aaf7d04de0f395df83a9a9874
Thorsten Hochreuter <thorsten.hochreuter@sap.com>
2023-07-05 16:08:50 +0200

    Best practice: Use sap.ui.core.IAsyncContentCreation interface in Component.js

--- app-ui5-webc/webapp/Component.js
+++ app-ui5-webc/webapp/Component.js
@@ -2,6 +2,7 @@ sap.ui.define(["sap/ui/core/UIComponent"], function(UIComponent) {
 	"use strict";
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
 		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
 		}
 	});
